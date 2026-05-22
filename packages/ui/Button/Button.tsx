f5f612334bbfaffab303485dae90bea1b82ac610
Serhii Berezin <serhii.berezin@sapient.pro>
2026-05-22 10:12:56 +0300

    change: packages/ui

--- packages/ui/Button/Button.tsx
+++ packages/ui/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { Button as RadixButton, type ButtonProps as RadixButtonProps } from '@radix-ui/themes'
 
+// comment to trigger project-a and project-b
 export type ButtonProps = RadixButtonProps
 export const Button = (props: ButtonProps) => {
   return (
