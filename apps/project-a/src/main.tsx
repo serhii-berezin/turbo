0ed8c8657638457a2a6ec2fbec3a009f50ee668a
Serhii Berezin <serhii.berezin@sapient.pro>
2026-05-22 10:09:52 +0300

    change: project-a

--- apps/project-a/src/main.tsx
+++ apps/project-a/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
 
+// Some comment
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Theme accentColor='green'>
